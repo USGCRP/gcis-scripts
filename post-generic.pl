6c687ebd4762a56f19f058519d01e45e7e9a016d
Kathryn Tipton <ktipton@usgcrp.gov>
2016-10-24 16:12:53 -0400

    Added template POD to scripts without POD
    Bumps USGCRP/gcis#463

--- post-generic.pl
+++ post-generic.pl
@@ -1,5 +1,53 @@
 #!/usr/bin/perl
 
+=head1 NAME
+
+[FILENAME] -- [ONE LINE DESCRIPTION]
+
+=head1 DESCRIPTION
+
+[FULL EXPLANATION OF THE SCRIPT. Remember to explain the
+'Why' and 'How' as well as the 'What'. Make note of any
+externalities, such as GCIS (very common), CrossRef.org,
+IO files, etc ]
+
+
+=head1 SYNOPSIS
+
+[GENERIC SCRIPT RUN e.g.: "./FILENAME [OPTIONS] < FOO.TXT"]
+
+=head1 OPTIONS
+
+=over
+
+=item <stdin>
+
+[STDIN DESCRIPTION (if used)]
+
+=item B<--[FOO]>
+
+[FOO DESCRIPTION]
+
+=item B<--BAR>
+
+[BAR DESCRIPTION]
+
+=item B<--verbose>
+
+Verbose option [IF USED; HIGHLY ENCOURAGED]
+
+=item B<--dry_run>
+
+Dry run [IF USED; HIGHLY ENCOURAGED]
+
+=back
+
+=head1 EXAMPLES
+
+[REALISTIC SCRIPT RUN e.g. `./FILENAME --foo --verbose <input.txt`]
+
+=cut
+
 use Gcis::Client;
 
 my $c = Gcis::Client->connect(url => 'http://localhost:3000');
--- togeneric2.pl
+++ togeneric2.pl
@@ -1,4 +1,53 @@
 #!/usr/bin/env perl
+
+=head1 NAME
+
+[FILENAME] -- [ONE LINE DESCRIPTION]
+
+=head1 DESCRIPTION
+
+[FULL EXPLANATION OF THE SCRIPT. Remember to explain the
+'Why' and 'How' as well as the 'What'. Make note of any
+externalities, such as GCIS (very common), CrossRef.org,
+IO files, etc ]
+
+
+=head1 SYNOPSIS
+
+[GENERIC SCRIPT RUN e.g.: "./FILENAME [OPTIONS] < FOO.TXT"]
+
+=head1 OPTIONS
+
+=over
+
+=item <stdin>
+
+[STDIN DESCRIPTION (if used)]
+
+=item B<--[FOO]>
+
+[FOO DESCRIPTION]
+
+=item B<--BAR>
+
+[BAR DESCRIPTION]
+
+=item B<--verbose>
+
+Verbose option [IF USED; HIGHLY ENCOURAGED]
+
+=item B<--dry_run>
+
+Dry run [IF USED; HIGHLY ENCOURAGED]
+
+=back
+
+=head1 EXAMPLES
+
+[REALISTIC SCRIPT RUN e.g. `./FILENAME --foo --verbose <input.txt`]
+
+=cut
+
 # This creates a generic publication for the references having reftypes which do not exist in our 
 # data model. The generic publication is subequently associated with the reference.
 use v5.14; use Gcis::Client; use Data::Dumper; use Getopt::Long; use Pod::Usage; use strict; 
