9d74c528fcc185a0fe15083e3fdfa1b5a5445c21
Kathryn Tipton <ktipton@usgcrp.gov>
2017-10-20 10:08:53 -0400

    Figure caption tbib connector
    Extended the reference connector via textual <tbib>s to handle
    Figure caption fields

--- populating_scripts/by_resource/references/connect_references_from_tbibs.pl
+++ populating_scripts/by_resource/references/connect_references_from_tbibs.pl
@@ -2,7 +2,7 @@
 
 =head1 NAME
 
-    connect_findings_to_references.pl -- Ensure references associated with findings match tbib tags.
+    connect_references_from_tbibs.pl -- Ensure references associated with findings match tbib tags.
 
 =head1 OPTIONS
 
@@ -18,13 +18,25 @@
 
     Chapter number. Optional
 
+=head2 B<--resource>
+
+    Which resource to search for tbibs & connect references to.
+
+    Valid options:
+      finding (searches : statement, uncertainties, evidence, confidence, process)
+      figure  (searches : caption)
+
+=head2 B<--remove_references>
+
+    Boolean. Will remove any references not found in the text from a resource.
+
 =head2 B<--dry_run>, B<n>
 
     Dry run.
 
 =head1 EXAMPLES
 
-    connect_findings_to_references.pl -u http://localhost:3000 -r nca3 -c 5
+    connect_references_from_tbibs.pl -u http://localhost:3000 --report nca3 -c 5 --resource finding
 
 =cut
 
@@ -32,88 +44,120 @@ use Getopt::Long qw/GetOptions/;
 use Data::Dumper;
 use Encode;
 use Pod::Usage;
-no warnings 'uninitialized';
 
 use Gcis::Client;
 
 use v5.14;
 
 GetOptions(
-  'url=s'            => \(my $url),
-  'report=s'         => \(my $report),
-  'chapter_number=s' => \(my $chapter_number),
-  'dry_run|n'        => \(my $dry_run),
-  'help'             => sub { pod2usage(verbose => 1 ) },
+  'url=s'             => \(my $url),
+  'report=s'          => \(my $report),
+  'chapter_number=s'  => \(my $chapter_number),
+  'resource=s'        => \(my $resource_type),
+  'dry_run|n'         => \(my $dry_run),
+  'remove_references' => \(my $remove_refs),
+  'help'              => sub { pod2usage(verbose => 1 ) },
 ) or die pod2usage("invalid options.");
 
 die "missing url" unless $url;
 die "missing report" unless $report;
+die "missing resource" unless $resource_type;
+die "invalid resource" unless ( $resource_type eq 'figure' || $resource_type eq 'finding' );
 
 my $c = Gcis::Client->new;
 
 $c->url($url);
 $c->find_credentials->login;
 
-my @findings;
-if ( $chapter_number ) {
-    @findings = $c->findings(report => $report, chapter_number => $chapter_number);
-} else {
-    @findings = $c->findings(report => $report);
-}
+my @resources = get_resources();
 
-my @fields = qw/statement uncertainties evidence confidence process/;
+my $fields = {
+    finding => [
+        "statement",
+        "uncertainties",
+        "evidence",
+        "confidence",
+        "process"
+    ],
+    figure  => [
+        "caption",
+    ],
+};
 
-for my $f (@findings) {
-    my $finding_uri = $f->{uri} or die "missing uri";
-    my $finding = $c->get_form($f);
+for my $r (@resources) {
+    my $resource_uri = $r->{uri} or die "missing uri";
+    my $resource = $c->get_form($r);
 
-    #say "Finding:" . Dumper $f;
-    #say "Finding form:" . Dumper $finding;
+    #say "Resource" . Dumper $r;
+    #say "Resource ($resource) form:" . Dumper $resource;
     my @uuids;
-    say "finding $finding->{identifier}";
-    for my $field (@fields) {
-        local $_ = $finding->{$field} or next;
-        push @uuids, $_ =~ m[<tbib>([a-z0-9-]+)</tbib>]g;
+    say "resource ($resource_type) $resource->{identifier}";
+    for my $field (@{ $fields->{$resource_type} }) {
+        my $field_value = $resource->{$field};
+        next unless $field_value;
+        #say "Analyzing $resource_uri field $field: $field_value";
+        push @uuids, $field_value =~ m[<tbib> *([a-z0-9-]+) *</tbib>]g;
     }
 
     #say "UUIDs:" . Dumper \@uuids;
 
-    my $finding_with_refs = $c->get($finding_uri);
-    #say "Finding with refs" . Dumper $finding_with_refs;
-    my %extra = map {
+    my $resource_with_refs = $c->get($resource_uri);
+    #say "resource with refs" . Dumper $resource_with_refs;
+    my %extraneous = map {
         # /reference/uuid => 1
         $_->{uri} => 1
-    } @{ $finding_with_refs->{references} };
-    #say "Initial Extras: " . Dumper \%extra;
+    } @{ $resource_with_refs->{references} };
+    #say "Initial Extras: " . Dumper \%extraneous;
 
     say "# uuids in text : ".@uuids;
-    # Associate references with this finding.
+    # Associate references with this resource.
     for my $refid (@uuids) {
-        delete $extra{"/reference/$refid"};
+        delete $extraneous{"/reference/$refid"};
         my $ref = $c->get("/reference/$refid");
         ##say "Ref for $refid: " . Dumper $ref;
-        if (grep { $finding_uri eq $_ } @{ $ref->{publications} } ){
+        if (grep { $resource_uri eq $_ } @{ $ref->{publications} } ){
             say "already connected to $refid";
         }
         elsif ($dry_run) {
             say "ready to post to /reference/rel/$refid";
         } else {
-            say "$finding_uri -> /reference/$refid";
-            $c->post("/reference/rel/$refid.json", { add_subpubref_uri => $finding_uri }) or warn $c->error;
+            say "$resource_uri -> /reference/$refid";
+            $c->post("/reference/rel/$refid.json", { add_subpubref_uri => $resource_uri }) or warn $c->error;
         }
     }
-    #say "Final Extras: " . Dumper \%extra;
+    #say "Final Extras: " . Dumper \%extraneous;
 
-    for my $extra (keys %extra) {
-        say "extra : $extra";
-        my $uri = $extra;
+    for my $extraneous (keys %extraneous) {
+        unless ( $remove_refs ) {
+            say "Not removing extraneous reference $extraneous";
+            next;
+        }
+        say "extraneous reference to remove: $extraneous";
+        my $uri = $extraneous;
         $uri =~ s[reference/][reference/rel/];
         if ($dry_run) {
-            say "ready to post to $uri to remove $finding_uri";
+            say "ready to post to $uri to remove $resource_uri";
         } else {
-            $c->post("$uri.json", { delete_subpub => $finding_uri }) or warn $c->error;
+            $c->post("$uri.json", { delete_subpub => $resource_uri }) or warn $c->error;
         }
     }
 }
 
 
+sub get_resources {
+    if ( $resource_type eq "finding" ) {
+        if ( $chapter_number ) {
+            return $c->findings(report => $report, chapter_number => $chapter_number);
+        } else {
+            return $c->findings(report => $report);
+        }
+    }
+    elsif ( $resource_type eq "figure" ) {
+        if ( $chapter_number ) {
+            return $c->figures(report => $report, chapter_number => $chapter_number);
+        } else {
+            return $c->figures(report => $report);
+        }
+    }
+    return;
+}
