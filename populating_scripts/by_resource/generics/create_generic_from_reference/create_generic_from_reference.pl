27c3b2a6fc881d7f7d673baba95e5862eaa230bf
Kathryn Tipton <ktipton@usgcrp.gov>
2019-01-18 10:40:12 -0500

    Initial script cleanup

--- populating_scripts/by_resource/generics/create_generic_from_reference/create_generic_from_reference.pl
+++ populating_scripts/by_resource/generics/create_generic_from_reference/create_generic_from_reference.pl
@@ -2,24 +2,24 @@
 
 =head1 NAME
 
-to-generic - creates child pubs of class "generic" for non-standard reference types, associates them with reference
+create_generic_from_reference.pl - creates & links generic pubs to refs
 
 =head1 SYNOPSIS
 
-togeneric [options]
+create_generic_from_reference [options]
 
   Options:
-    -type refers to the type of reference to be updated.
-    -url refers to the URL of the GCIS instance.
-    -max_update is the maximum number of entries to update. 
-    -dry_run is a flag that indicates a dry run.
-    -help provides a brief help message.
+    --type refers to the type of reference to be updated.
+    --url refers to the URL of the GCIS instance.
+    --max_update is the maximum number of entries to update. 
+    --dry_run is a flag that indicates a dry run.
+    --help provides a brief help message.
 
 =head1 OPTIONS
 
 =over 8
 
-=item B<-type>
+=item B<--type>
 
 the type of reference to be updated (default is cproc)
 
@@ -33,19 +33,19 @@ the allowed values are:
     thesis   : Thesis
     film     : Film or Broadcast
 
-=item B<-url>
+=item B<--url>
 
 the URL of the GCIS instance (default is the dev instance)
 
-=item B<-max_update>
+=item B<--max_update>
 
 the maximum number of entries to update (default is 1 entry)
 
-=item B<-dry_run>
+=item B<--dry_run>
 
 a flag that indicates a dry run (default is to update the instance)
 
-=item B<-help>
+=item B<--help>
 
 prints a help message and exits
 
@@ -53,15 +53,13 @@ prints a help message and exits
 
 =head1 DESCRIPTION
 
-B<togeneric.pl> creates child publications of class 'generic' for reference types of
-nonstandard reference type classes.  The child pubs are subsequently associcated with these
-references.  The program is designed to allow users to select how many new child pubs to
-create, and displays the title and UUID pertaining to each new child pub entry generated.
+B<create_generic_from_reference.pl> creates child publications of class
+'generic' for reference types of nonstandard reference type classes.  The child
+pubs are subsequently associcated with these references.  The program is
+designed to allow users to select how many new child pubs to create, and
+displays the title and UUID pertaining to each new child pub entry generated.
 
 =cut
-
-# This creates a generic publication for the references having reftypes which do not exist in our data model.
-# The generic publication is subequently associated with the reference.
 
 use v5.14;
 use Gcis::Client;
@@ -87,25 +85,28 @@ my $url   = "https://data-stage.globalchange.gov";
 my $max_update = 100;
 my $dry_run = 0;
 my $help = 0;
- my $result = GetOptions ("type=s" => \$type, 
-                    "url=s" => \$url,
-                    "max_update=i"   => \$max_update,
-                    "dry_run"  => \$dry_run,
-                    'help|?' => \$help);
+
+my $result = GetOptions (
+    "type=s"         => \$type,
+    "url=s"          => \$url,
+    "max_update=i"   => \$max_update,
+    "dry_run"        => \$dry_run,
+    'help|?'         => \$help
+);
 
 pod2usage(-verbose => 2) if $help;
- if (!grep $type eq $_, keys %types) {
-     say "invalid type : $type";
-     pod2usage(-verbose => 2);
- }
+
+if (!grep $type eq $_, keys %types) {
+    say "invalid type : $type";
+    pod2usage(-verbose => 2);
+}
 
 say " type : $type, $types{$type}";
-print " url $url\n";
+say " url $url";
 say " max update : $max_update";
 say " dry run" if $dry_run;
 
 my $all = "?all=1";
-# my $all;
 my $ref_search = "/reference.json$all";
 
 my $search = $url.$ref_search;
@@ -117,6 +118,7 @@ my $n = @$refs;
 say " n refs : $n";
 my $n_update = 0;
 my $i=0;
+
 for (@$refs) {
     next if $_->{attrs}->{reftype} ne $types{$type};
     next if $_->{child_publication_id};
@@ -128,7 +130,7 @@ for (@$refs) {
      $sub->($_, $generic_pub);
 
      say "updating title : $_->{attrs}->{Title}, uri : $_->{uri}";
-    
+
     if ($dry_run) {
         say "would have updated this reference";
         next;
@@ -140,32 +142,29 @@ for (@$refs) {
     $ref_form->{publication_uri} = $g->get($ref_form->{publication_uri})->{uri};
     delete $ref_form->{sub_publication_uris};
 
-    $g->post("/reference/$_->{identifier}", $ref_form) or die $g->error;  
-  
+    $g->post("/reference/$_->{identifier}", $ref_form) or die $g->error;
 }
 
-# sub marticle_copy {
- #    my ($ref, $pub) = @_;
-  #   for (qw(reftype Magazine ISSN Author Date Title Pages Volume Publisher Year URL)) {
-   #      $pub->{attrs}->{$_} = $ref->{attrs}->{$_};
-    # }
+sub marticle_copy {
+    my ($ref, $pub) = @_;
+    for (qw(reftype Magazine ISSN Author Date Title Pages Volume Publisher Year URL)) {
+        $pub->{attrs}->{$_} = $ref->{attrs}->{$_};
+    }
+    $pub->{attrs}->{Issue} = $ref->{attrs}->{'Issue Number'};
+    $pub->{attrs}->{'Place Published'} = $ref->{attrs}->{'Place Published'};
+    return;
+}
 
-     # $pub->{attrs}->{Issue} = $ref->{attrs}->{'Issue Number'};
-     # $pub->{attrs}->{'Place Published'} = $ref->{attrs}->{'Place Published'};
-     # return;
- # }
-
- # sub narticle_copy {
-   #  my ($ref, $pub) = @_;
-    # for (qw(reftype Newspaper Title Pages Year URL)) {
-     #    $pub->{attrs}->{$_} = $ref->{attrs}->{$_};
-     # }
-
-     # $pub->{attrs}->{Date} = $ref->{attrs}->{'Issue Date'};
-     # $pub->{attrs}->{Author} = $ref->{attrs}->{'Reporter'};
-     # $pub->{attrs}->{'Place Published'} = $ref->{attrs}->{'Place Published'};
-     # return;
- # }
+sub narticle_copy {
+   my ($ref, $pub) = @_;
+   for (qw(reftype Newspaper Title Pages Year URL)) {
+       $pub->{attrs}->{$_} = $ref->{attrs}->{$_};
+   }
+   $pub->{attrs}->{Date} = $ref->{attrs}->{'Issue Date'};
+   $pub->{attrs}->{Author} = $ref->{attrs}->{'Reporter'};
+   $pub->{attrs}->{'Place Published'} = $ref->{attrs}->{'Place Published'};
+   return;
+}
 
  sub earticle_copy {
      my ($ref, $pub) = @_;
@@ -179,50 +178,50 @@ for (@$refs) {
      return;
   }
 
- # sub cpaper_copy {
-   #  my ($ref, $pub) = @_;
-   #  for (qw(reftype Author Date Year Title DOI URL)) {
-    #     $pub->{attrs}->{$_} = $ref->{attrs}->{$_};
-    # }
+sub cpaper_copy {
+   my ($ref, $pub) = @_;
+   for (qw(reftype Author Date Year Title DOI URL)) {
+       $pub->{attrs}->{$_} = $ref->{attrs}->{$_};
+   }
 
-    # $pub->{attrs}->{'Conference Name'} = $ref->{attrs}->{'Conference Name'};
-    # $pub->{attrs}->{'Conference Location'} = $ref->{attrs}->{'Conference Location'};
-    # return;
-  # }
+    $pub->{attrs}->{'Conference Name'} = $ref->{attrs}->{'Conference Name'};
+    $pub->{attrs}->{'Conference Location'} = $ref->{attrs}->{'Conference Location'};
+    return;
+}
 
- # sub cproc_copy {
-   #  my ($ref, $pub) = @_;
-    # for (qw(reftype Author Title Date DOI URL)) {
-     #    $pub->{attrs}->{$_} = $ref->{attrs}->{$_};
-     # }
+sub cproc_copy {
+    my ($ref, $pub) = @_;
+    for (qw(reftype Author Title Date DOI URL)) {
+        $pub->{attrs}->{$_} = $ref->{attrs}->{$_};
+    }
 
-     # $pub->{attrs}->{'Year of Conference'} = $ref->{attrs}->{'Year of Conference'};
-     # $pub->{attrs}->{'Conference Name'} = $ref->{attrs}->{'Conference Name'};
-     # $pub->{attrs}->{'Conference Location'} = $ref->{attrs}->{'Conference Location'};
-     # return;
-  # }
+    $pub->{attrs}->{'Year of Conference'} = $ref->{attrs}->{'Year of Conference'};
+    $pub->{attrs}->{'Conference Name'} = $ref->{attrs}->{'Conference Name'};
+    $pub->{attrs}->{'Conference Location'} = $ref->{attrs}->{'Conference Location'};
+    return;
+}
 
- # sub thesis_copy {
-  #   my ($ref, $pub) = @_;
-   #  for (qw(reftype Author Date University Year Title DOI URL)) {
-    #     $pub->{attrs}->{$_} = $ref->{attrs}->{$_};
-    # }
+sub thesis_copy {
+    my ($ref, $pub) = @_;
+    for (qw(reftype Author Date University Year Title DOI URL)) {
+        $pub->{attrs}->{$_} = $ref->{attrs}->{$_};
+    }
 
-    # $pub->{attrs}->{'Academic Department'} = $ref->{attrs}->{'Academic Department'};
-    # $pub->{attrs}->{'Number of Pages'} = $ref->{attrs}->{'Number of Pages'};
-    # return;
-  # }
+    $pub->{attrs}->{'Academic Department'} = $ref->{attrs}->{'Academic Department'};
+    $pub->{attrs}->{'Number of Pages'} = $ref->{attrs}->{'Number of Pages'};
+    return;
+}
 
-#  sub film_copy {
- #    my ($ref, $pub) = @_;
-  #   for (qw(reftype Director Year Title Producer URL)) {
-   #      $pub->{attrs}->{$_} = $ref->{attrs}->{$_};
-   #  }
+sub film_copy {
+    my ($ref, $pub) = @_;
+        for (qw(reftype Director Year Title Producer URL)) {
+        $pub->{attrs}->{$_} = $ref->{attrs}->{$_};
+    }
 
-   #  $pub->{attrs}->{'Series Title'} = $ref->{attrs}->{'Series Title'};
-   #  $pub->{attrs}->{'Date Released'} = $ref->{attrs}->{'Date Released'};
-   #  return;
- # }
+    $pub->{attrs}->{'Series Title'} = $ref->{attrs}->{'Series Title'};
+    $pub->{attrs}->{'Date Released'} = $ref->{attrs}->{'Date Released'};
+    return;
+}
 
 say "done";
 
